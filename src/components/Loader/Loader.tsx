1f5b6f7f4bcef875bf2e73bd78005bdd97549eb3
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-05-17 03:01:50 -0600

    Loader implement (50%)

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import styles from './Loader.module.css';
+
+export const Loader = () => {
+  return (
+    <div className="loader-container">
+    <div className="loader">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+    </div>
+  </div>
+  )
+}
