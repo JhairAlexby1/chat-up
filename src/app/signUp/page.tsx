23fcf4269fd43d60ad3b356157087d73dedadf7d
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-05-18 08:55:28 -0600

    feat: Add lazy loading for images in SignUp page

--- src/app/signUp/page.tsx
+++ src/app/signUp/page.tsx
@@ -1,9 +1,20 @@
-import { FormSignUp } from "@/components/formSignUp/FormSignUp"
-
+import { FormSignUp } from "@/components/formSignUp/FormSignUp";
+import Image from "next/image";
 
 export default function SignUp() {
-    return (
-        <>
-            <FormSignUp />
-        </>
-    )};
+  return (
+    <>
+      <div>
+        <Image
+          className="mx-4 rounded-3xl"
+          src="/images/registerImage.jpg"
+          width={333}
+          height={333}
+          alt="Picture of the author"
+        />
+      </div>
+
+      <FormSignUp />
+    </>
+  );
+}
