5e2541c189a17b2ec484f5fe4f1a6b6c5b775097
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-05-18 10:23:53 -0600

    chore: Update login page with image and form styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,21 @@
-import { FormLogin } from "@/components/formLogin/FormLogin";
+import Image from "next/image";
+import styles from "./login.module.css";
+import {FormLogin}  from "@/components/formLogin/FormLogin";
 
 export default function Login() {
     return (
-        <>
-            <FormLogin />
-        </>
+        <div className={styles.container}>
+      <div>
+        <Image
+          className="mx-4 rounded-3xl"
+          src="/images/registerImage.jpg"
+          width={333}
+          height={333}
+          alt="Picture of the author"
+        />
+      </div>
+      
+        <FormLogin />
+      
+    </div>
     )}
--- src/components/formLogin/FormLogin.tsx
+++ src/components/formLogin/FormLogin.tsx
@@ -1,6 +1,79 @@
+"use client"
+import Link from "next/link";
+import { useState } from "react";
+import styles from "./FormLogin.module.css";
 
 export const FormLogin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  function btnLogin(): void {
+    alert(email + ' ' + password);
+    <Link href=""></Link> //Pendiente
+  }
+
+
   return (
-    <div className="text-white">Componente FormLogin</div>
+    <div className={styles.containerForm}>
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="relative flex flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
+          <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            Crea Una Nueva Cuenta
+          </h4>
+
+          <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+            <div className="mb-4 flex flex-col gap-6">
+              
+              <div className="relative h-11 w-full min-w-[200px]">
+                <input
+                  className="peer h-full w-full rounded-md border border-violet-700	 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-violet-700	 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  placeholder=" "
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-violet-700	 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-violet-700	 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  Correo Electrónico
+                </label>
+              </div>
+              <div className="relative h-11 w-full min-w-[200px]">
+                <input
+                  type="password"
+                  className="peer h-full w-full rounded-md border border-violet-700	 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-violet-700	 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  placeholder=" "
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-violet-700	 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-violet-700	 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  Contrasena
+                </label>
+              </div>
+            </div>
+    
+
+            <button onClick={btnLogin}
+              className="mt-6 block w-full select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-light="true"
+            >
+              Iniciar Sesión
+            </button>
+
+            <Link href="/landingPage">
+              <button
+                className="mt-6 block w-full select-none rounded-lg bg-stone-50 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="button"
+                data-ripple-light="true"
+              >
+                Regresar
+              </button>
+            </Link>
+            
+            
+          </form>
+          
+          </div>
+        </div>
+      </div>
   )
 }
