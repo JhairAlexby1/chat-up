b2dff3b0a06697a190a350e6b24e900a28ff3752
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-05-23 04:58:30 -0600

    RegisteredPersons se modifico para que aqui se implementara el short Polling

--- src/components/registeredPersons/RegisteredPersons.tsx
+++ src/components/registeredPersons/RegisteredPersons.tsx
@@ -1,24 +1,33 @@
-"use client"
-import { useState } from 'react';
-import styles from './RegisteredPersons.module.css';
+"use client";
+//aqui estoy usando short polling , aqui el usuario tiene que solicitar los datos cada rato para ver el numero actualizado
+import { useState } from "react";
+import axios from 'axios';
+import styles from "./RegisteredPersons.module.css";
 
 export const RegisteredPersons = () => {
-  const [ups, setUps] = useState([11]); 
-  const [showUps, setShowUps] = useState(false);
+  const [userCount, setUserCount] = useState(0);
+  const [showCount, setShowCount] = useState(false);
 
   const handleClick = () => {
-    setShowUps(true);
-  }
+    axios.get('http://localhost:3000/usuarios')
+      .then(response => {
+        setUserCount(response.data.length);
+        setShowCount(true);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  };
 
   return (
     <div className={styles.contenedor}>
-      <h1>Da click para saber cuantos UPS somos :)</h1>
-      <button className={styles.button} onClick={handleClick}>Mostrar UPS</button>
-
-      {showUps && ups.map((up, index) => (
-        <p className={styles.let} key={index}>UPS {up}</p>
-      ))}
-
+      <h1 className="text-3xl font-bold text-blue-500 hover:text-blue-700 transition-colors duration-300 ">
+        Da click para saber cuantos UPS somos :)
+      </h1>{" "}
+      <button className={styles.button} onClick={handleClick}>
+        Mostrar UPS
+      </button>
+      {showCount && <p className={styles.let}>Total de usuarios: {userCount}</p>}
     </div>
-  )
-}
+  );
+};
