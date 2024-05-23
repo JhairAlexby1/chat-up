ae6dbe079d9754bc872264727bfb97e7bd689efd
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-05-23 03:58:34 -0600

    short polling implementado con css modificaco en ConnectedPeople, pero no esta completo ya que se muestra solo la cantidad de usuarios registrados y no conectados

--- src/components/connectedPeople/ConnectedPeople.tsx
+++ src/components/connectedPeople/ConnectedPeople.tsx
@@ -1,8 +1,36 @@
+"use client"
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import styles from "./ConnectedPeople.module.css";
 import Image from "next/image";
 
+interface Person {
+  nombre: string;
+}
+
 export const ConnectedPeople = () => {
-  const people = ['Persona 1', 'Persona 2', 'jhair', 'jhair', 'jhair'];
+  const [people, setPeople] = useState<Person[]>([]);
+
+  
+
+  useEffect(() => {
+    //short  ppolling 
+    const fetchPeople = async () => {
+      try {
+        const response = await axios.get<Person[]>('http://localhost:3000/usuarios');
+        setPeople(response.data);
+      } catch (error) {
+        console.error('Error fetching people:', error);
+      }
+    };
+
+    fetchPeople();
+
+    // cad 5 segndos 
+    const intervalId = setInterval(fetchPeople, 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className={styles.contenedor}>
@@ -11,19 +39,19 @@ export const ConnectedPeople = () => {
       <h2 className={styles.textConect}>Personas Conectadas</h2>
 
       {people.map((person, index) => (
-        <div key={index} className={styles.personas}>
-          <Image
-            className={styles.imgPersona}
-            src="/images/photosPeople/persona.jpg"
-            width={30}
-            height={30}
-            alt={`Picture of ${person}`}
-            priority
-          />
+  <div key={index} className={styles.personas}>
+    <Image
+      className={styles.imgPersona}
+      src="/images/photosPeople/persona.jpg"
+      width={30}
+      height={30}
+      alt={`Picture of ${person.nombre}`} 
+      priority
+    />
 
-          <p>{person}</p>
-        </div>
-      ))}
+    <p className='text-white'>{person.nombre}</p> 
+  </div>
+))}
     </div>
   );
 };
