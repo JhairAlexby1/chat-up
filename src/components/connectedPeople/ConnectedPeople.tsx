fc6e4477cf8325312201ae764f7c1e06a60ca1e2
Jhair Alejandro Cruz Palacios <223244@ids.upchiapas.edu.mx>
2024-06-07 14:57:52 -0600

    long polling implementado en connectedPeople

--- src/components/connectedPeople/ConnectedPeople.tsx
+++ src/components/connectedPeople/ConnectedPeople.tsx
@@ -1,6 +1,5 @@
-"use client"
-//aqui implemento long polling para que se actualice la lista de personas conectadas en tiempo real
-//falta modificarlo para que solo muestre los que estan en linea
+"use client";
+
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from "./ConnectedPeople.module.css";
@@ -13,27 +12,30 @@ interface Person {
 export const ConnectedPeople = () => {
   const [people, setPeople] = useState<Person[]>([]);
 
+  const fetchConnectedPeople = async () => {
+    try {
+      const response = await axios.get<{ usuariosConectados: Person[] }>('http://localhost:3001/usuarios/esperarNotificaciones', {
+        withCredentials: true,
+        headers: {
+          'Authorization': `Bearer ${document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1")}` // Obtener token de la cookie
+        }
+      });
+      setPeople(response.data.usuariosConectados);
+      fetchConnectedPeople(); // recursividad para actualiar la lista de personas conectadas 
+    } catch (error) {
+      console.error('Error fetching connected people:', error);
+      setTimeout(fetchConnectedPeople, 5000); // por si hay error reinicia a los 5 se
+    }
+  };
+
   useEffect(() => {
-    const fetchPeople = async () => {
-      try {
-        const response = await axios.get<Person[]>('http://localhost:3001/usuarios');
-        setPeople(response.data);
-      } catch (error) {
-        console.error('Error fetching people:', error);
-      }
-    };
-
-    const intervalId = setInterval(fetchPeople, 5000); 
-
-    return () => clearInterval(intervalId); 
+    fetchConnectedPeople();
   }, []);
 
   return (
     <div className={styles.contenedor}>
       <h1>UP CHAT</h1>
-
       <h2 className={styles.textConect}>Personas Conectadas</h2>
-
       {people.map((person, index) => (
         <div key={index} className={styles.personas}>
           <Image
@@ -41,11 +43,10 @@ export const ConnectedPeople = () => {
             src="/images/photosPeople/persona.jpg"
             width={30}
             height={30}
-            alt={`Picture of ${person.nombre}`} 
+            alt={`Picture of ${person.nombre}`}
             priority
           />
-
-          <p className='text-white'>{person.nombre}</p> 
+          <p className='text-white'>{person.nombre}</p>
         </div>
       ))}
     </div>
